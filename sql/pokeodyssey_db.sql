90bb9486b9d3f9b2db6a5e541cbc84654a9c753f
naimyc_ubu <naimyc-desktop>
2025-01-22 15:51:19 +0100

    Sql Update

--- sql/pokeodyssey_db.sql
+++ sql/pokeodyssey_db.sql
@@ -2,10 +2,10 @@ create database pokeodyssey_db;
 
 create table
     mc_users (
-        user_id int AUTO_INCREMENT,
+        user_id int NOT NULL UNIQUE,
         xuuid int NOT NULL UNIQUE,
         mc_edition int NOT NULL,
-
+        
         balance DECIMAL DEFAULT 0.0,
         
         name varchar(25),
@@ -17,14 +17,15 @@ create table
         
         isBanned TINYINT (1) DEFAULT 0,
         isMuted TINYINT (1) DEFAULT 0,
-        isRegistered TINYINT (1) DEFAULT 0,
-        isActive TINYINT (1) DEFAULT 1,
+        isRegistered TINYINT (1) DEFAULT 0,         isActive TINYINT (1) DEFAULT 1,
         
-        registerDate DATETIME NOT NULL DEFAULT NOW (),
+        registerDate DATETIME DEFAULT NOW(),
+
         PRIMARY KEY (user_id)
     );
 
-INSERT INTO
-    mc_users (xuuid, mc_edition, discord_name)
-VALUES
-    (545645644, 0, "Byakko");
+INSERT INTO mc_users 
+(user_id, xuuid, mc_edition, discord_name)
+SELECT IFNULL(MAX(user_id), 0) + 1,
+     545645644, 0, "Byakko"
+FROM mc_users;
